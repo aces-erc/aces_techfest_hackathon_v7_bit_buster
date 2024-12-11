8be68e3fe0722985c5a62cea76a3b83d8acd0217
TechExplorerAnish <anishghimire178@gmail.com>
2024-12-11 22:32:59 +0545

    add login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Landing from './pages/landing'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
--- Frontend/src/components/landing/hero.jsx
+++ Frontend/src/components/landing/hero.jsx
@@ -1,30 +1,29 @@
 import { Button } from "@/components/ui/button"
+import DialogDemo from "../auth/loginForm";
 
 
 const  HeroSection=()=> {
   return (
-    <div className="relative min-h-screen overflow-hidden">
+    <div className="relative my-4 overflow-hidden">
       <div className="absolute inset-0">
         <img
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover sm:object-scale-down"
           src="https://www.cpsmumbai.org/Uploads/2762023161833920.png"
           alt="Blood donation"
         />
         <div className="absolute inset-0 mix-blend-multiply opacity-40" />
       </div>
-      <div className="relative px-4 sm:px-6 lg:px-8 py-24 md:py-32 bg-primary/50">
+      <div className="relative px-4 sm:px-6 lg:px-8 py-24 md:py-32 bg-primary/40 rounded-sm">
         <div className="md:w-2/3 lg:w-1/2">
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-extrabold text-secondary leading-tight">
             Give the gift of life:
             <span className="block text-primary-">Donate Blood Today</span>
           </h1>
-          <p className="mt-6 text-xl md:text-2xl text-gray-100 max-w-3xl">
+          <p className="mt-6 text-xl md:text-2xl text-secondary/70 max-w-3xl">
             Your donation can save up to three lives. Join our community of heroes and make a difference in someone's life today. Every drop counts!
           </p>
           <div className="mt-10 flex flex-col sm:flex-row gap-4">
-            <Button className="w-full sm:w-auto flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-8">
-              Sign In
-            </Button>
+            <DialogDemo/>
             <Button variant="outline" className="w-full sm:w-auto flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-100 bg-opacity-20 hover:bg-opacity-30 md:py-4 md:text-lg md:px-10">
               Sign Up
             </Button>
