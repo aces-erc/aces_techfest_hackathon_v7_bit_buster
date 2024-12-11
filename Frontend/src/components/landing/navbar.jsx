d722a3ae97fabf7a698f5ddb4bc1daaaeb8a2820
Kaushal Rijal <kaushalrijal091@gmail.com>
2024-12-11 21:00:52 +0545

    create landing navbar

--- Frontend/src/components/landing/navbar.jsx
+++ Frontend/src/components/landing/navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import {Button} from "@/components/ui/button"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex flex-row w-full justify-between'>
+        <div>
+            <h1 className='font-bold text-3xl text-tert'>
+                RedBond
+            </h1>
+        </div>
+        <div>
+            <ul className='flex flex-row gap-4'>
+                <li><Button variant="outline">SignUp</Button></li>
+                <li><Button>Login</Button></li>
+            </ul>
+        </div>
+    </div>
   )
 }
 
