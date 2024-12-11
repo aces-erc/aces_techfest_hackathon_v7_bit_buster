cd078e47fe1a5be3a48a218bf288e40edd9864de
Kaushal Rijal <kaushalrijal091@gmail.com>
2024-12-11 21:40:49 +0545

    update font and styles

--- Frontend/src/components/landing/box.jsx
+++ Frontend/src/components/landing/box.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Box = (props) => {
   return (
     <div className='bg-gray-200 py-4 px-6 m-2 rounded-md shadow-md'>
-        <h1 className='font-semibold text-2xl'>
+        <h1 className='font-semibold text-xl text-left '>
             {props.title}
         </h1>
-        <p className='py-2'>
+        <p className='py-2 text-sm text-left'>
             {props.description}
         </p>
     </div>
--- Frontend/src/components/landing/why.jsx
+++ Frontend/src/components/landing/why.jsx
@@ -8,8 +8,6 @@ const reasons = [
     {title: "Unique Resource", description: "Blood cannot be manufactured; it can only come from generous donors like you."},
     {title: "Health Benefits", description: "Regular blood donation can reduce iron levels and lower the risk of certain health issues."},
     {title: "Free Health Screening", description: "Every time you donate, your blood is tested for various conditions, giving you insights into your health."},
-    {title: "Support Life-Saving Research", description: "Donated blood can also be used for medical research to improve treatments and discover cures."},
-    {title: "Help Rare Blood Groups", description: "Rare blood types are especially needed, and your donation could be crucial for someone with a rare type."},
 ];
 
 
@@ -17,11 +15,12 @@ const Why = () => {
   return (
     <div className='flex flex-col w-full'>
         <div>
-            <h1 className='text-4xl font-semibold'>
+            <h1 className='text-3xl font-semibold text-left'>
                 <span className='text-blue-900'>Why</span> donate blood
             </h1>
         </div>
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
+        <hr></hr>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 my-2'>
                 {reasons.map((reason) => {
                     return(
                             <Box title={reason.title} description={reason.description} />
