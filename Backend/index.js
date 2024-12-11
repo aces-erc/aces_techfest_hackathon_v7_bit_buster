086385f527869227cfa2311f0bc2840e1aeecf81
Prabin1025y <acharyaprabin1025y@gmail.com>
2024-12-11 18:42:43 +0545

    Added nodemon envfile and dotenv

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,16 @@
-const express = require("express")
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
 
 const app = express();
+const PORT = process.env.PORT;
 
-const PORT = 8000
 
 app.get("/", (req, res) => {
-    res.status(200).json({"Message": "Hello world"});
-})
+  res.status(200).json({ Message: "Hello world" });
+});
 
-app.listen(PORT, ()=>{
-    console.log("Listening on port", PORT)
-})
+app.listen(PORT, () => {
+  console.log("Listening on port", PORT);
+});
