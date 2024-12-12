0c85cd8047f4c5a8ee5005fe79d619376e7d2490
Kaushal Rijal <kaushalrijal091@gmail.com>
2024-12-12 09:36:04 +0545

    update navbar

--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useLocation, Link } from "react-router-dom";
 
 const Navbar = () => {
+
+    const location = useLocation(); // once ready it returns the 'window.location' object
+  const [url, setUrl] = useState(null);
+  useEffect(() => {
+    setUrl(location.pathname);
+    console.log(url)
+  }, [location]);
+
   return (
-    <div className="flex flex-row w-full justify-between">
+    <div className="flex flex-row w-full justify-between items-center">
       <div>
         <h1 className="font-bold text-4xl text-tert">RedBond</h1>
       </div>
-      <div className="flex flex-row justify-between w-1/3">
-        <ul className="flex flex-row gap-4">
-          <li>
-            <Button variant="outline">SignUp</Button>
-          </li>
-          <li>
-            <Button>Login</Button>
-          </li>
-        </ul>
+      <div className="flex flex-row justify-between w-2/5 items-center">
+        <nav className="flex flex-row gap-4">
+          <Link to="/donors" className={`${url === "/donors" ? "underline underline-offset-8 font-semibold" : ""}`}>View Donors</Link>
+          <Link to="/request" className={`${url === "/request" ? "underline underline-offset-8 font-semibold" : ""}`}>Request Blood</Link>
+          <Link to="/appointment" className={`${url === "/appointment" ? "underline underline-offset-8 font-semibold" : ""}`}>Appointments</Link>
+        </nav>
         <Avatar>
           <AvatarImage src="https://static.wikia.nocookie.net/beluga-characters/images/6/64/Beluga4.jpg" alt="@shadcn" />
           <AvatarFallback>CN</AvatarFallback>
