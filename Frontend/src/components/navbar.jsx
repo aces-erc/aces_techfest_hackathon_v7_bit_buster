919673e2cdbb096af634e0c21c1cbb7c00b9eee7
Kaushal Rijal <kaushalrijal091@gmail.com>
2024-12-12 09:16:50 +0545

    add components

--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = () => {
   return (
-    <div className='flex flex-row w-full justify-between'>
-        <div>
-            <h1 className='font-bold text-4xl text-tert'>
-                RedBond
-            </h1>
-        </div>
-        <div>
-            <ul className='flex flex-row gap-4'>
-                <li><Button variant="outline">SignUp</Button></li>
-                <li><Button>Login</Button></li>
-            </ul>
-        </div>
+    <div className="flex flex-row w-full justify-between">
+      <div>
+        <h1 className="font-bold text-4xl text-tert">RedBond</h1>
+      </div>
+      <div className="flex flex-row justify-between w-1/3">
+        <ul className="flex flex-row gap-4">
+          <li>
+            <Button variant="outline">SignUp</Button>
+          </li>
+          <li>
+            <Button>Login</Button>
+          </li>
+        </ul>
+        <Avatar>
+          <AvatarImage src="https://static.wikia.nocookie.net/beluga-characters/images/d/d9/Beluga.webp/revision/latest/scale-to-width-down/250?cb=20220929123514" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { UseAuthStore } from '../zustand/AuthStore'
 import { Navigate } from 'react-router-dom';
-import Navbar from '../components/navbar';
 
 const Home = () => {
   const {authUser} = UseAuthStore();
   return (
     <>
     {!authUser && (<Navigate to="/" replace={true} />)}
+    <h1>Hello world</h1>
     </>
   )
 }
