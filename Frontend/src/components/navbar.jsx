081eec36e7c379cfc1acf9f80fd07477619edce1
TechExplorerAnish <anishghimire178@gmail.com>
2024-12-12 11:49:17 +0545

    refactor: enhance navbar layout and improve link styling

--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -5,29 +5,55 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useLocation, Link } from "react-router-dom";
 
 const Navbar = () => {
-
-    const location = useLocation(); // once ready it returns the 'window.location' object
+  const location = useLocation(); // once ready it returns the 'window.location' object
   const [url, setUrl] = useState(null);
   useEffect(() => {
     setUrl(location.pathname);
-    console.log(url)
+    console.log(url);
   }, [location]);
 
   return (
-    <div className="flex flex-row w-full justify-between items-center">
+    <div className="flex flex-col sm:flex-row w-full justify-between items-center">
       <div>
         <h1 className="font-bold text-4xl text-tert">RedBond</h1>
       </div>
-      <div className="flex flex-row justify-between w-2/5 items-center">
-        <nav className="flex flex-row gap-4">
-          <Link to="/donors" className={`${url === "/donors" ? "underline underline-offset-8 font-semibold" : ""}`}>View Donors</Link>
-          <Link to="/request" className={`${url === "/request" ? "underline underline-offset-8 font-semibold" : ""}`}>Request Blood</Link>
-          <Link to="/appointment" className={`${url === "/appointment" ? "underline underline-offset-8 font-semibold" : ""}`}>Appointments</Link>
+      <div className="flex flex-row justify-bw-2/5 items-center">
+        <nav className="flex flex-col sm:flex-row gap-4">
+          <Link
+            to="/donors"
+            className={`min-w-fit justify-center self-center ${
+              url === "/donors"
+                ? "underline underline-offset-8 font-semibold"
+                : ""
+            }`}
+          >
+            View Donors
+          </Link>
+          <Link
+            to="/request"
+            className={`min-w-fit justify-center self-center${
+              url === "/request"
+                ? "underline underline-offset-8 font-semibold"
+                : ""
+            }`}
+          >
+            Request Blood
+          </Link>
+          <Link
+            to="/appointment"
+            className={`min-w-fit justify-center self-center${
+              url === "/appointment"
+                ? "underline underline-offset-8 font-semibold"
+                : ""
+            }`}
+          >
+            Appointments
+          </Link>
+          <Link className="flex justify-center align-middle gap-2">
+            <ContextMenu />
+            <span className="self-center  sm:hidden">username</span>
+          </Link>
         </nav>
-        <Avatar>
-          <AvatarImage src="https://static.wikia.nocookie.net/beluga-characters/images/6/64/Beluga4.jpg" alt="@shadcn" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
       </div>
     </div>
   );
